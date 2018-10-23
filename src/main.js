081bab5a0e137e32c2c3a4565e06c8a97ee6de79
swain <iamswain@hotmail.com>
2018-10-23 00:41:07 -0700

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
+import '@babel/polyfill'
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import './registerServiceWorker'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
